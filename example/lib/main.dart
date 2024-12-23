c8a7a3a047bdbdd1d7ff51969c15e03d3bb306eb
zmuranaka <zmuranaka@gmail.com>
2024-12-23 16:40:31 -0700

    Update pusbpec & ran 'dart fix --apply' again

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:pinch_zoom/pinch_zoom.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,31 +15,33 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: ExamplePage(),
+      home: const ExamplePage(),
     );
   }
 }
 
 class ExamplePage extends StatelessWidget {
+  const ExamplePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('PinchZoom Page'),
+        title: const Text('PinchZoom Page'),
       ),
       body: Column(
         children: [
           PinchZoom(
-            child: Image.network('https://placekitten.com/640/360'),
             maxScale: 2.5,
             onZoomStart: (){print('Start zooming cat');},
             onZoomEnd: (){print('Stop zooming cat');},
+            child: Image.network('https://placekitten.com/640/360'),
           ),
           PinchZoom(
-            child: Image.network('https://placedog.net/640/360'),
             maxScale: 2.5,
             onZoomStart: (){print('Start zooming dog');},
             onZoomEnd: (){print('Stop zooming dog');},
+            child: Image.network('https://placedog.net/640/360'),
           ),
         ],
       ),
